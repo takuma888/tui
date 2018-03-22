c4dd305e910e6719dfee0aa3349ea8a7a3463faa
tachigo <641979199@qq.com>
2018-03-22 15:48:44 +0800

    reset

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ require __DIR__ . '/../config.php';
  * @var \Composer\Autoload\ClassLoader $autoLoader
  */
 $autoLoader = require VENDOR_ROOT . '/autoload.php';
-$autoLoader->add('TCG', ROOT . '/framework');
 $autoLoader->add('Tachigo', SRC_ROOT);
 
 require __DIR__ . '/UIKernel.php';
